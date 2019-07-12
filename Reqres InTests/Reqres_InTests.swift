142e837a8f0ac5997eab6b446c57c397b8455bba
Suprianto D <ersaka96@gmail.com>
2019-07-12 13:07:56 +0800

    Initial Commit

---
+++ Reqres InTests/Reqres_InTests.swift
@@ -0,0 +1,34 @@
+//
+//  Reqres_InTests.swift
+//  Reqres InTests
+//
+//  Created by Suprianto Djamalu on 12/07/19.
+//  Copyright © 2019 Suprianto Djamalu. All rights reserved.
+//
+
+import XCTest
+@testable import Reqres_In
+
+class Reqres_InTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
